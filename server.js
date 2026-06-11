514923812d3a637e0a570e7de63d2a563eb5d4f3
greftty <bibleway@bellnet.ca>
2026-06-11 11:26:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
   'https://bellbusinesscanada.netlify.app',
   'https://sunny-pastelito-8843b7.netlify.app',
   'https://viralbell.netlify.app',
-  'https://fruad.vercel.app',
+  'https://fratty.vercel.app',
   'https://wwbell.vercel.app',
    // add all your trusted frontends here
 ];
